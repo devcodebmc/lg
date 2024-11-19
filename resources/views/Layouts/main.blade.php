a5c33534413dc7f30eafc87c068d352bd77e3107
Brayan Manzano <al221610173@gmail.com>
2024-11-19 14:11:16 -0600

    error and success mesages

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -13,6 +13,34 @@
 
     @include('components.navbar')
 
+    @if (session('success'))
+        <div class="uk-alert-success uk-position-z-index uk-align-center uk-text-center" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="uk-alert-danger uk-position-z-index uk-align-center uk-text-center" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            {{ session('error') }}
+        </div>
+    @endif
+
+    @if (session('success-cot'))
+        <div class="uk-alert-success uk-position-z-index uk-align-center uk-text-center" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            {{ session('success-cot') }}
+        </div>
+    @endif
+
+    @if (session('error-cot'))
+        <div class="uk-alert-danger uk-position-z-index uk-align-center uk-text-center" uk-alert>
+            <a class="uk-alert-close" uk-close></a>
+            {{ session('error-cot') }}
+        </div>
+    @endif
+
     <section>
         @yield('content')
     </section> 
