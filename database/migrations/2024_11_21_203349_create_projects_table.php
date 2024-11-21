5806e55878a860758d681e44a575e2048d87bdfc
Brayan Manzano <al221610173@gmail.com>
2024-11-21 17:33:16 -0600

    new migrations(projects and secondary_images) table

---
+++ database/migrations/2024_11_21_203349_create_projects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('location');
+            $table->string('manager');
+            $table->string('type');
+            $table->dateTime('delivery_date');
+            $table->string('cover_image')->nullable();
+            $table->string('cover_video')->nullable();
+            $table->json('finishes')->nullable(); // Para almacenar los acabados
+            $table->json('proceedings')->nullable(); // Para almacenar los procesos
+            $table->timestamps(); // created_at and updated_at
+            $table->softDeletes(); // deleted_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
