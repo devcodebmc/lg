2f3dd46b5fef480806854607c473094dd5e51db0
Brayan Manzano <al221610173@gmail.com>
2024-12-03 16:36:45 -0600

    styles

--- resources/views/partials/description.blade.php
+++ resources/views/partials/description.blade.php
@@ -4,20 +4,25 @@
         <div class="uk-width-1-2@m uk-position-relative uk-custom-images">
             <img src="https://images.pexels.com/photos/11427524/pexels-photo-11427524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Imagen en blanco y negro" class="uk-box-shadow-medium uk-border-rounded imagen-desc-1">
             <img src="https://images.pexels.com/photos/4981810/pexels-photo-4981810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Imagen en color" class="uk-box-shadow-medium uk-border-rounded imagen-desc-2">
+            
+            <div class="uk-margin-xlarge-top">
+                <h2 class="uk-heading-small uk-text-bold lg-desc">Nuestra Misión</h2>
+                <p>
+                    Nuestra misión es convertir tus sueños en realidad, brindando servicios de alta calidad que superen tus expectativas. Creemos en el poder del diseño y la construcción para mejorar la calidad de vida, y nos esforzamos por entregar proyectos que reflejen tu personalidad y estilo.
+                </p>
+            </div>
+            
         </div>
 
         <!-- Texto y lista de características -->
         <div class="uk-width-1-2@m">
-            <h2 class="uk-heading-small uk-text-bold lg-desc">
+            <h2 class="uk-heading-small uk-text-bold ">
                 Trabajo De Calidad Que <br> Cumplen Tus Expectativas
             </h2>
             <p style="font-size: 1rem;">
                 En <strong>Construcción y Remodelación LG</strong>, somos un equipo de apasionados profesionales dedicados a transformar espacios, creando ambientes únicos y funcionales. Con más de una década de experiencia en el sector, nos enorgullecemos de ofrecer soluciones integrales de construcción y remodelación que combinan calidad, innovación y atención al detalle.
             </p>
-            <h2 class="uk-heading-small uk-text-bold lg-desc">Nuestra Misión</h2>
-            <p>
-                Nuestra misión es convertir tus sueños en realidad, brindando servicios de alta calidad que superen tus expectativas. Creemos en el poder del diseño y la construcción para mejorar la calidad de vida, y nos esforzamos por entregar proyectos que reflejen tu personalidad y estilo.
-            </p>
+           
             
             <ul class="uk-list uk-list-large uk-list-divider uk-padding-remove-left lg-list">
                 <li><span uk-icon="crosshairs"></span> Experiencia.</li>
